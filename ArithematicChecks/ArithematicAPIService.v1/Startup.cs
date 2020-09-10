155fb35e98cf1e80b1909a0438f72dd7cd2e1948
Dileep Bhat <dilipmbhat@gmail.com>
2020-09-10 19:36:07 +0530

    feat(swagger):Introduce Swagger documentation

--- ArithematicChecks/ArithematicAPIService.v1/Startup.cs
+++ ArithematicChecks/ArithematicAPIService.v1/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ArithematicAPIService.v1
 {
@@ -26,6 +27,9 @@ namespace ArithematicAPIService.v1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(
+                c => c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "ArithematicAPI", Version = "v1" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +45,14 @@ namespace ArithematicAPIService.v1
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(
+                c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    c.RoutePrefix = string.Empty;
+                }
+                );
 
             app.UseEndpoints(endpoints =>
             {
